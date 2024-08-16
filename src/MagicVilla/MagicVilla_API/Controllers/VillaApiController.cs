52c448b4edfa30dc83988c1dbf90ce0cedad3200
Saquib Hassan <hsakib1244@outlook.com>
2024-08-16 20:37:42 +0600

    villaDto

--- src/MagicVilla/MagicVilla_API/Controllers/VillaApiController.cs
+++ src/MagicVilla/MagicVilla_API/Controllers/VillaApiController.cs
@@ -1,10 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MagicVilla_API.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_API.Controllers
 {
     [ApiController]
     public class VillaApiController : ControllerBase
     {
+        [Route("api/VillaApi")]
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>()
+            {
+                new Villa() {Id=1, Name="Beach view"},
+                new Villa() {Id=2, Name="Pool view" }
+            };
+        }
 
     }
 }
+    
---
+++ src/MagicVilla/MagicVilla_API/Models/Dto/VillaDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MagicVilla_API.Models.Dto
+{
+    public class VillaDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
