98b5847122bd57312e4d3856e46de07ea5651464
Saquib Hassan <hsakib1244@outlook.com>
2024-08-18 03:51:20 +0600

    Connectionstring established

---
+++ src/MagicVilla/MagicVilla_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using MagicVilla_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
--- src/MagicVilla/MagicVilla_API/Models/Villa.cs
+++ src/MagicVilla/MagicVilla_API/Models/Villa.cs
@@ -1,7 +1,12 @@
-﻿namespace MagicVilla_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MagicVilla_API.Models
 {
     public class Villa
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Details { get; set; }
