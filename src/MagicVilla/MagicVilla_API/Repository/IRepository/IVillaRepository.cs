280d9166f56e276dc8e2ab6d5bb629395e35188e
Saquib Hassan <hsakib1244@outlook.com>
2024-08-19 02:55:39 +0600

    IRepository interface added

---
+++ src/MagicVilla/MagicVilla_API/Repository/IRepository/IVillaRepository.cs
@@ -0,0 +1,14 @@
+﻿using MagicVilla_API.Models;
+using System.Linq.Expressions;
+
+namespace MagicVilla_API.Repository.IRepository
+{
+    public interface IVillaRepository
+    {
+        Task Create(Villa entity);
+        Task Remove(Villa entity);
+        Task Save();
+        Task<List<Villa>> GetAll(Expression<Func<Villa>> filter = null);
+        Task<Villa> GetVilla(Expression<Func<Villa>> filter = null, bool tracked = true);
+    }
+}
