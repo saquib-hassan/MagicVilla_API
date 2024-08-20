275759a970a09045e3012cc96d52e81aa9f743c4
Saquib Hassan <hsakib1244@outlook.com>
2024-08-20 21:42:30 +0600

    learnToSave

--- src/MagicVilla/MagicVilla_API/Repository/IRepository/IRepository.cs
+++ src/MagicVilla/MagicVilla_API/Repository/IRepository/IRepository.cs
@@ -9,7 +9,7 @@ namespace MagicVilla_API.Repository.IRepository
         Task RemoveAsync(T entity);
         Task SaveAsync();
         
-        Task<List<Villa>> GetAllAsync(Expression<Func<T, bool>> filter = null);
-        Task<Villa> GetAsync(Expression<Func<T, bool>> filter = null, bool tracked = true);
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter = null);
+        Task<T> GetAsync(Expression<Func<T, bool>>? filter = null, bool tracked = true);
     }
 }
--- src/MagicVilla/MagicVilla_API/Repository/VillaRepository.cs
+++ src/MagicVilla/MagicVilla_API/Repository/VillaRepository.cs
@@ -14,56 +14,11 @@ namespace MagicVilla_API.Repository
         {
             _db = db;
         }
-        public async Task CreateAsync(Villa entity)
+       public async Task UpdateAsync(Villa entity)
         {
-            await _db.Villas.AddAsync(entity);
-            await SaveAsync();
-        }
-
-        public async Task<List<Villa>> GetAllAsync(Expression<Func<Villa, bool>> filter = null)
-        {
-             IQueryable<Villa> query = _db.Villas;
-
-            if (filter != null)
-            {
-                query = query.Where(filter);
-            }
-            return await query.ToListAsync();
-        }
-
-        public async Task<Villa> GetAsync(Expression<Func<Villa, bool>> filter = null, bool tracked = true)
-        {
-            IQueryable<Villa> query = _db.Villas;
-
-            if(!tracked)
-            {
-                query = query.AsNoTracking();
-            }
-
-            if (filter != null)
-            {
-                query = query.Where(filter);
-            }
-            return await query.FirstOrDefaultAsync();
-        }
-
-       
-        public async Task RemoveAsync(Villa entity)
-        {
-             _db.Villas.Remove(entity);
-           await SaveAsync();
-            
-        }
-
-        public async Task SaveAsync()
-        {
-             _db.SaveChangesAsync();
-        }
-
-        public async Task UpdateAsync(Villa entity)
-        {
+            entity.UpdatedDate = DateTime.Now;
             _db.Update(entity);
-            await SaveAsync();
+            await _db.SaveChangesAsync();
         }
     }
 }
