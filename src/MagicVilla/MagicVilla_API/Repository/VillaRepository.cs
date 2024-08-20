9c9e70843739c0119f7e1b0b15dc1b440ce8bd9c
Saquib Hassan <hsakib1244@outlook.com>
2024-08-20 11:59:45 +0600

    implemeted

---
+++ src/MagicVilla/MagicVilla_API/Repository/VillaRepository.cs
@@ -0,0 +1,34 @@
+﻿using MagicVilla_API.Models;
+using MagicVilla_API.Repository.IRepository;
+using System.Linq.Expressions;
+
+namespace MagicVilla_API.Repository
+{
+    public class VillaRepository : IVillaRepository
+    {
+        public Task Create(Villa entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Villa>> GetAll(Expression<Func<Villa, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Villa> GetVilla(Expression<Func<Villa, bool>> filter = null, bool tracked = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Remove(Villa entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
