ece467b47c13d9b75c10db2392d43047007552f4
Saquib Hassan <hsakib1244@outlook.com>
2024-08-20 18:54:31 +0600

    not working

--- src/MagicVilla/MagicVilla_API/Repository/IRepository/IVillaRepository.cs
+++ src/MagicVilla/MagicVilla_API/Repository/IRepository/IVillaRepository.cs
@@ -5,11 +5,7 @@ namespace MagicVilla_API.Repository.IRepository
 {
     public interface IVillaRepository
     {
-        Task CreateAsync(Villa entity);
-        Task RemoveAsync(Villa entity);
-        Task SaveAsync();
+    
         Task UpdateAsync(Villa entity);
-        Task<List<Villa>> GetAllAsync(Expression<Func<Villa,bool>> filter = null);
-        Task<Villa> GetAsync(Expression<Func<Villa,bool>> filter = null, bool tracked = true);
     }
 }
---
+++ src/MagicVilla/MagicVilla_API/Repository/Repository.cs
@@ -0,0 +1,15 @@
+﻿using MagicVilla_API.Data;
+using MagicVilla_API.Models;
+using MagicVilla_API.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+
+namespace MagicVilla_API.Repository
+{
+    public class Repository
+    {
+       
+    }
+}
