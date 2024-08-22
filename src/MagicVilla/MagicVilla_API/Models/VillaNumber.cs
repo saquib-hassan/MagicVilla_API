0c09191b2f20cbf81c7a88e0544c4e7a19e6db63
Saquib Hassan <hsakib1244@outlook.com>
2024-08-22 19:41:10 +0600

    ForignKey referrence added to the Villa

--- src/MagicVilla/MagicVilla_API/Models/VillaNumber.cs
+++ src/MagicVilla/MagicVilla_API/Models/VillaNumber.cs
@@ -7,6 +7,10 @@ namespace MagicVilla_API.Models
     {
         [Key,DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int VillaNo { get; set; }
+
+        [ForeignKey("Villas")]
+        public int VillaId { get; set; }
+        public Villa Villa { get; set; }
         public string SpecialDetails { get; set; }
         public DateTime CreateDate { get; set; }
         public DateTime UpdateDate { get; set; }
