4bfa111af507b802093f5ad8ad0727fd1565c09a
Saquib Hassan <hsakib1244@outlook.com>
2024-08-24 02:37:56 +0600

    Getting the Villa Numbers

---
+++ src/MagicVilla/MagicVilla_Web/Controllers/VillaNumberController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace MagicVilla_Web.Controllers
+{
+    public class VillaNumberController : Controller
+    {
+        protected readonly IVillaNumberService _villaNumberService;
+        protected readonly IMapper _mapper;
+        //protected readonly IHttpClientFactory _httpClientFactory;
+        public VillaNumberController(IVillaNumberService villaNumberService, IMapper mapper)
+        {
+            this._villaNumberService = villaNumberService;
+            this._mapper = mapper;
+        }
+        public async Task<IActionResult> IndexVillaNumber()
+        {
+            List<VillaNumberDTO> list = new();
+            var response = await _villaNumberService.GetAllAsync<APIResponse>();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<VillaNumberDTO>>(Convert.ToString(response.Result));
+            }
+
+            return View(list);
+        }
+    }
+}
