8512de45dab0903f2966e3c4b833885c4d1725d0
Saquib Hassan <hsakib1244@outlook.com>
2024-08-25 04:37:51 +0600

    migration problem in statistics table

--- src/MagicVilla/MagicVilla_API/Data/ApplicationDbContext.cs
+++ src/MagicVilla/MagicVilla_API/Data/ApplicationDbContext.cs
@@ -9,9 +9,9 @@ namespace MagicVilla_API.Data
         {
 
         }
-
         public DbSet<Villa> Villas { get; set; }
         public DbSet<VillaNumber> VillaNumbers { get; set; }
+        public DbSet<LocalUser> LocalUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
