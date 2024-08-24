0ab2e520c472b6ca576c650bae557a9e168d7e43
Saquib Hassan <hsakib1244@outlook.com>
2024-08-25 00:47:21 +0600

    Dropdown populated

--- src/MagicVilla/MagicVilla_Web/Controllers/VillaNumberController.cs
+++ src/MagicVilla/MagicVilla_Web/Controllers/VillaNumberController.cs
@@ -65,7 +65,18 @@ namespace MagicVilla_Web.Controllers
                 }
             }
 
+            var resp = await _villaService.GetAllAsync<APIResponse>();
+            if (resp != null && resp.IsSuccess)
+            {
+                model.VillaList = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(resp.Result)).
+                    Select(i => new SelectListItem
+                    {
+                        Text = i.Name,
+                        Value = i.Id.ToString()
+                    });
+            }
             return View(model);
+
         }
 
 
