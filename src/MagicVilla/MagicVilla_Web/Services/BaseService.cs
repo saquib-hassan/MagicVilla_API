b6fda469621ff0cc0cd6e47ba9115793ae50943c
Saquib Hassan <hsakib1244@outlook.com>
2024-08-25 00:36:06 +0600

    fixed

--- src/MagicVilla/MagicVilla_Web/Services/BaseService.cs
+++ src/MagicVilla/MagicVilla_Web/Services/BaseService.cs
@@ -55,23 +55,25 @@ namespace MagicVilla_Web.Services
 
                 try
                 {
-                    APIResponse APIResponse = JsonConvert.DeserializeObject<APIResponse>(apiContent);
+                    APIResponse ApiResponse = JsonConvert.DeserializeObject<APIResponse>(apiContent);
                     if(apiResponse.StatusCode==System.Net.HttpStatusCode.BadRequest
                         || apiResponse.StatusCode==System.Net.HttpStatusCode.NotFound)
                     {
-                        APIResponse.StatusCode = System.Net.HttpStatusCode.BadRequest;
-                        APIResponse.IsSuccess = false;
-                        var res = JsonConvert.SerializeObject(APIResponse);
+                        ApiResponse.StatusCode = System.Net.HttpStatusCode.BadRequest;
+                        ApiResponse.IsSuccess = false;
+                        var res = JsonConvert.SerializeObject(ApiResponse);
                         var returnObj = JsonConvert.DeserializeObject<T>(res);
                         return returnObj;
                     }
                 }
                 catch(Exception e) 
                 {
-                    var APIResponse = JsonConvert.DeserializeObject<T>(apiContent);
-                    return APIResponse;
+                    var exceptionResponse = JsonConvert.DeserializeObject<T>(apiContent);
+                    return exceptionResponse;
                 }
+                var APIResponse = JsonConvert.DeserializeObject<T> (apiContent);
                 
+                return APIResponse;
             }
 
             catch (Exception ex)
