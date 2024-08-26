f2d4c071ac92572b489f2d15c8de49f4ab4ea605
Saquib Hassan <hsakib1244@outlook.com>
2024-08-27 02:46:16 +0600

    UserRepo successfully implemented

--- src/MagicVilla/MagicVilla_API/Repository/IRepository/IUserRepository.cs
+++ src/MagicVilla/MagicVilla_API/Repository/IRepository/IUserRepository.cs
@@ -6,7 +6,7 @@ namespace MagicVilla_API.Repository.IRepository
     public interface IUserRepository
     {
         bool isUniqueUser(string username);
-        Task<LoginRequestDTO> Login (LoginRequestDTO loginRequestDTO);
+        Task<LoginResponseDTO> Login (LoginRequestDTO loginRequestDTO);
         Task<LocalUser> LocalUser (RegistrationRequestDTO registrationRequestDTO);
     }
 }
---
+++ src/MagicVilla/MagicVilla_API/Repository/UserRepository.cs
@@ -0,0 +1,85 @@
+﻿using MagicVilla_API.Data;
+using MagicVilla_API.Models;
+using MagicVilla_API.Models.Dto;
+using MagicVilla_API.Repository.IRepository;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MagicVilla_API.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public string secretkey;
+        public UserRepository(ApplicationDbContext db, IConfiguration configuration)
+        {
+            this._db = db;
+            secretkey = configuration.GetValue<string>("ApiSettings:Secret");
+        }
+        public bool isUniqueUser(string username)
+        {
+            var user = _db.LocalUsers.FirstOrDefault(u=>u.UserName == username);
+            if (username == null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<LocalUser> LocalUser(RegistrationRequestDTO registrationRequestDTO)
+        {
+            var user = new LocalUser()
+            {
+                UserName = registrationRequestDTO.UserName,
+                Password = registrationRequestDTO.Password,
+                Name = registrationRequestDTO.Name,
+                Role = registrationRequestDTO.Role
+            };
+
+            _db.LocalUsers.Add(user);
+            await _db.SaveChangesAsync();
+            user.Password = "";
+            return user;
+            
+        }
+
+        public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            var user = _db.LocalUsers.FirstOrDefault(u => u.UserName == loginRequestDTO.UserName &&
+            u.Password == loginRequestDTO.Password);
+
+            if(user == null)
+            {
+                return null;
+            }
+            //Need the JWT token here.
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secretkey);
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role)
+
+                }),
+
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            LoginResponseDTO loginResponseDTO = new LoginResponseDTO()
+            {
+                Token = tokenHandler.WriteToken(token),
+                User = user
+            };
+
+            return loginResponseDTO;
+        }
+    }
+}
