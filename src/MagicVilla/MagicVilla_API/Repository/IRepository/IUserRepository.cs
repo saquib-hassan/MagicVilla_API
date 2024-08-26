65888d1db55b782cbcfce8acb3c92992a16b873f
Saquib Hassan <hsakib1244@outlook.com>
2024-08-27 00:16:35 +0600

    interface of Repository created

---
+++ src/MagicVilla/MagicVilla_API/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using MagicVilla_API.Models;
+using MagicVilla_API.Models.Dto;
+
+namespace MagicVilla_API.Repository.IRepository
+{
+    public interface IUserRepository
+    {
+        bool isUniqueUser(string username);
+        Task<LoginRequestDTO> Login (LoginRequestDTO loginRequestDTO);
+        Task<LocalUser> LocalUser (RegistrationRequestDTO registrationRequestDTO);
+    }
+}
